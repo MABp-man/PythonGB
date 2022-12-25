fc5ecd6e3b9fe3f16561fd12e6cec7fb364f3bcb
MABp <mabp-man@k66.ru>
2022-12-26 01:07:55 +0200

    if operator included

---
+++ Lect1/if_operator.py
@@ -0,0 +1,17 @@
+a = int(input('a = '))
+b = int(input('b = '))
+
+if a > b:
+    print(a)
+else:
+    print(b)
+
+c = input('как тебя зовут? ')
+if c == 'саша':
+    print('Ура это же САШКА!!')
+elif c == 'паша':
+    print('Ура это же ПАШКА!!')
+elif c == 'вова':
+    print('Ура это же ВОВКА!!')
+else:
+    print(f'Ура это же {c}!!')
